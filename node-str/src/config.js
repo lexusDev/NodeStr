5b6782664f4e0a9a6563e4f5a7af12a48c448b00
Alex Monteiro <alex.monteiro@digiage.com.br>
2020-03-22 11:56:02 -0300

    First commit

---
+++ node-str/src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://localhost:27017/ndstr',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodedev;AccountKey=VG5UQsy66I3tcRbv7CZWH3o2JooFwJyhUgUNn7P0lXAMfL/d35ltcEPYTZUSQVg1tYo88hdjj9J6bMo9mDi5GA==;EndpointSuffix=core.windows.net'
+}
---
+++ node-str/src/models/customer.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required : true
+    },
+    roles:[{
+        type: String,
+        required: true,
+        enum:['user', 'admin'],
+        default: 'user'
+    }]
+});
+
+module.exports = mongoose.model("Customer", CustomerSchema);
